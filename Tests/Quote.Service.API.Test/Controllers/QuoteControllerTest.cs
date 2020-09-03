75f47baa02d486a8e89b6d0df22910d71d070117
Thirumalaivasan S <THIRUMALAIVASAN9245@GMAIL.COM>
2020-09-03 21:08:54 +0530

    Add files via upload

---
+++ Tests/Quote.Service.API.Test/Controllers/QuoteControllerTest.cs
@@ -0,0 +1,302 @@
+﻿namespace Quote.Service.API.Test.Controllers
+{
+    using AutoMapper;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using NUnit.Framework;
+    using Quote.Service.API.Domain.Entities;
+    using Quote.Service.API.Models.Enum;
+    using Quote.Service.API.Models.ResponseModels;
+    using Quote.Service.API.Models.ResquestModels;
+    using Quote.Service.API.Service.RequestHandlers.CommandHandlers;
+    using Quote.Service.API.Service.RequestHandlers.QueryHandlers;
+    using Quote.Servive.API.Controllers;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class QuoteControllerTest
+    {
+        private Mock<IMediator> mediatR;
+
+        private QuoteController controller;
+
+        private Mapper mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            mediatR = new Mock<IMediator>();
+            var config = new MapperConfiguration(m =>
+            {
+                m.CreateMap<CreateQuoteModel, Quote>();
+                m.CreateMap<UpdateQuoteModel, Quote>();
+                m.CreateMap<Quote, ResponseQuoteModel>();
+            });
+            mapper = new Mapper(config);
+            var quote = new Quote
+            {
+                Id = Guid.NewGuid(),
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = 1,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                QuoteDate = new DateTime(2020, 08, 08),
+                RetirementAge = 62,
+                InvestmentAmount = 100000,
+                MaturityAmount = 1500,
+                PensionPlan = 1
+            };
+            var quoteList = new List<Quote> { quote };
+            mediatR.Setup(m => m.Send(It.IsAny<CreateQuoteRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(quote));
+            mediatR.Setup(m => m.Send(It.IsAny<UpdateQuoteRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(quote));
+            mediatR.Setup(m => m.Send(It.IsAny<GetQuoteByIdRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(quote));
+            mediatR.Setup(m => m.Send(It.IsAny<GetQuoteRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(quoteList));
+            controller = new QuoteController(mapper, mediatR.Object);
+        }
+
+        [Test]
+        public async Task PostAsync()
+        {
+            // Act
+            var createQuoteModel = new CreateQuoteModel
+            {
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = ClientSex.Male,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                RetirementAge = 62,
+                InvestmentAmount = 100000,
+                PensionPlan = PensionPlan.PensionSilver
+            };
+            var result = await controller.Post(createQuoteModel) as CreatedResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(201, result.StatusCode);
+            var responseQuoteModel = result.Value as ResponseQuoteModel;
+            Assert.NotNull(responseQuoteModel);
+            Assert.AreEqual("ClientName 1", responseQuoteModel.ClientName);
+        }
+
+        [Test]
+        public async Task PutAsync()
+        {
+            // Act
+            var updateQuoteModel = new UpdateQuoteModel
+            {
+                Id = new Guid("6877D1A3-4BC7-4D0B-B802-08D844D525AC"),
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = ClientSex.Male,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                RetirementAge = 62,
+                InvestmentAmount = 100000,
+                PensionPlan = PensionPlan.PensionSilver
+            };
+            var result = await controller.Put(updateQuoteModel) as OkObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+            var responseQuoteModel = result.Value as ResponseQuoteModel;
+            Assert.NotNull(responseQuoteModel);
+            Assert.AreEqual("ClientName 1", responseQuoteModel.ClientName);
+        }
+
+        [Test]
+        public async Task DeleteAsync()
+        {
+            // Act
+            var quoteId = new Guid("6877D1A3-4BC7-4D0B-B802-08D844D525AC");
+            var result = await controller.Delete(quoteId) as OkObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+            var responseQuoteModel = result.Value as ResponseQuoteModel;
+            Assert.NotNull(responseQuoteModel);
+            Assert.AreEqual("ClientName 1", responseQuoteModel.ClientName);
+        }
+
+        [Test]
+        public async Task GetAsync()
+        {
+            // Act
+            var result = await controller.Get() as OkObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+            var responseQuoteModel = result.Value as List<ResponseQuoteModel>;
+            Assert.NotNull(responseQuoteModel);
+            Assert.AreEqual(1, responseQuoteModel.Count);
+        }
+
+        [Test]
+        public async Task PostNullMediatorResponseAsync()
+        {
+            // Act
+            var createQuoteModel = new CreateQuoteModel
+            {
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = ClientSex.Male,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                RetirementAge = 62,
+                InvestmentAmount = 100000,
+                PensionPlan = PensionPlan.PensionSilver
+            };
+            mediatR.Setup(m => m.Send(It.IsAny<CreateQuoteRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult<Quote>(null));
+            controller = new QuoteController(mapper, mediatR.Object);
+            var result = await controller.Post(createQuoteModel) as ObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(409, result.StatusCode);
+            Assert.AreEqual("Error Occurred While Creating a Quote", result.Value);
+        }
+
+        [Test]
+        public async Task GetNullMediatorResponseAsync()
+        {
+            // Act
+            mediatR.Setup(m => m.Send(It.IsAny<GetQuoteRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult<List<Quote>>(null));
+            controller = new QuoteController(mapper, mediatR.Object);
+            var result = await controller.Get() as ObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(404, result.StatusCode);
+            Assert.AreEqual("No Quote found", result.Value);
+        }
+
+        [Test]
+        public async Task DeleteNullMediatorResponseAsync()
+        {
+            // Act
+            var quoteId = new Guid("6877d1a3-4bc7-4d0b-b802-08d844d525ac");
+            mediatR.Setup(m => m.Send(It.IsAny<GetQuoteByIdRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult<Quote>(null));
+            controller = new QuoteController(mapper, mediatR.Object);
+            var result = await controller.Delete(quoteId) as BadRequestObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(400, result.StatusCode);
+            Assert.AreEqual("No quote found with the id 6877d1a3-4bc7-4d0b-b802-08d844d525ac", result.Value);
+        }
+
+        [Test]
+        public async Task PutNullMediatorResponseAsync()
+        {
+            // Act
+            var updateQuoteModel = new UpdateQuoteModel
+            {
+                Id = new Guid("6877D1A3-4BC7-4D0B-B802-08D844D525AC"),
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = ClientSex.Male,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                RetirementAge = 62,
+                InvestmentAmount = 100000,
+                PensionPlan = PensionPlan.PensionSilver
+            };
+            mediatR.Setup(m => m.Send(It.IsAny<GetQuoteByIdRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult<Quote>(null));
+            controller = new QuoteController(mapper, mediatR.Object);
+            var result = await controller.Put(updateQuoteModel) as BadRequestObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(400, result.StatusCode);
+            Assert.AreEqual("No quote found with the id 6877d1a3-4bc7-4d0b-b802-08d844d525ac", result.Value);
+        }
+
+        [Test]
+        public async Task GetThrowsExceptionAsync()
+        {
+            // Act
+            mediatR.Setup(m => m.Send(It.IsAny<GetQuoteRequest>(), It.IsAny<CancellationToken>())).Throws(new InvalidOperationException());
+            controller = new QuoteController(mapper, mediatR.Object);
+            var result = await controller.Get() as ObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(400, result.StatusCode);
+            Assert.AreEqual("Operation is not valid due to the current state of the object.", result.Value);
+        }
+
+        [Test]
+        public async Task PostThrowsExceptionAsync()
+        {
+            // Act
+            var createQuoteModel = new CreateQuoteModel
+            {
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = ClientSex.Male,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                RetirementAge = 62,
+                InvestmentAmount = 100000,
+                PensionPlan = PensionPlan.PensionSilver
+            };
+            mediatR.Setup(m => m.Send(It.IsAny<CreateQuoteRequest>(), It.IsAny<CancellationToken>())).Throws(new InvalidOperationException());
+            controller = new QuoteController(mapper, mediatR.Object);
+            var result = await controller.Post(createQuoteModel) as ObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(400, result.StatusCode);
+            Assert.AreEqual("Operation is not valid due to the current state of the object.", result.Value);
+        }
+
+        [Test]
+        public async Task PutThrowsExceptionAsync()
+        {
+            // Act
+            var updateQuoteModel = new UpdateQuoteModel
+            {
+                Id = new Guid("6877D1A3-4BC7-4D0B-B802-08D844D525AC"),
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = ClientSex.Male,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                RetirementAge = 62,
+                InvestmentAmount = 100000,
+                PensionPlan = PensionPlan.PensionSilver
+            };
+            mediatR.Setup(m => m.Send(It.IsAny<GetQuoteByIdRequest>(), It.IsAny<CancellationToken>())).Throws(new InvalidOperationException());
+            controller = new QuoteController(mapper, mediatR.Object);
+            var result = await controller.Put(updateQuoteModel) as ObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(400, result.StatusCode);
+            Assert.AreEqual("Operation is not valid due to the current state of the object.", result.Value);
+        }
+
+        [Test]
+        public async Task DeleteThrowsExceptionAsync()
+        {
+            // Act
+            var quoteId = new Guid("6877d1a3-4bc7-4d0b-b802-08d844d525ac");
+            mediatR.Setup(m => m.Send(It.IsAny<GetQuoteByIdRequest>(), It.IsAny<CancellationToken>())).Throws(new InvalidOperationException());
+            controller = new QuoteController(mapper, mediatR.Object);
+            var result = await controller.Delete(quoteId) as ObjectResult;
+
+            // Assert      
+            Assert.NotNull(result);
+            Assert.AreEqual(400, result.StatusCode);
+            Assert.AreEqual("Operation is not valid due to the current state of the object.", result.Value);
+        }
+    }
+}
---
+++ Tests/Quote.Service.API.Test/Infrastructure/AutoMapper/MappingProfileTest.cs
@@ -0,0 +1,172 @@
+﻿namespace Quote.Service.API.Test.Infrastructure.AutoMapper
+{
+    using FluentAssertions;
+    using global::AutoMapper;
+    using NUnit.Framework;
+    using Quote.Service.API.Domain.Entities;
+    using Quote.Service.API.Infrastructure.AutoMapper;
+    using Quote.Service.API.Models.Enum;
+    using Quote.Service.API.Models.ResponseModels;
+    using Quote.Service.API.Models.ResquestModels;
+    using System;
+
+    public class MappingProfileTests
+    {
+        private CreateQuoteModel createQuoteModel;
+        private UpdateQuoteModel updateQuoteModel;
+        private Quote quoteEntity;
+        private IMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            _mapper = mockMapper.CreateMapper();
+
+            createQuoteModel = new CreateQuoteModel
+            {
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = ClientSex.Male,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                RetirementAge = 66,
+                InvestmentAmount = 300000,
+                PensionPlan = PensionPlan.PensionPlatinum
+            };
+            updateQuoteModel = new UpdateQuoteModel
+            {
+                Id = Guid.Parse("9f35b48d-cb87-4783-bfdb-21e36012930a"),
+                ClientName = "ClientName 1",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = ClientSex.Male,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                RetirementAge = 66,
+                InvestmentAmount = 100000,
+                PensionPlan = PensionPlan.PensionSilver
+            };
+
+            quoteEntity = new Quote
+            {
+                Id = Guid.NewGuid(),
+                ClientName = "ClientName 2",
+                DateofBirth = new DateTime(1992, 09, 09),
+                ClientSex = 1,
+                Email = "Test@test.com",
+                MobileNumber = "1234567880",
+                QuoteDate = new DateTime(2020, 08, 08),
+                RetirementAge = 66,
+                InvestmentAmount = 100000,
+                MaturityAmount = 1500,
+                PensionPlan = 1
+            };
+        }
+
+        [Test]
+        public void Map_Quote_CreateQuoteModel_ShouldHaveValidConfig()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+               cfg.CreateMap<Quote, CreateQuoteModel>());
+
+            configuration.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void Map_Quote_UpdateQuoteModel_ShouldHaveValidConfig()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+                cfg.CreateMap<Quote, UpdateQuoteModel>());
+
+            configuration.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void Map_ResponseQuoteModel_Quote_ShouldHaveValidConfig()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+                cfg.CreateMap<Quote, ResponseQuoteModel>());
+
+            configuration.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void Map_Quote_Quote_ShouldHaveValidConfig()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+                cfg.CreateMap<Quote, Quote>());
+
+            configuration.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void Map_CreateQuoteModel_Quote()
+        {
+            var quote = _mapper.Map<Quote>(createQuoteModel);
+
+            quote.ClientName.Should().Be(createQuoteModel.ClientName);
+            quote.ClientSex.Should().Be((int)createQuoteModel.ClientSex);
+            quote.DateofBirth.Should().Be(createQuoteModel.DateofBirth);
+            quote.Email.Should().Be(createQuoteModel.Email);
+            quote.MobileNumber.Should().Be(createQuoteModel.MobileNumber);
+            quote.InvestmentAmount.Should().Be(createQuoteModel.InvestmentAmount);
+            quote.PensionPlan.Should().Be((int)createQuoteModel.PensionPlan);
+            quote.RetirementAge.Should().Be(createQuoteModel.RetirementAge);
+        }
+
+        [Test]
+        public void Map_UpdateQuoteModel_Quote()
+        {
+            var quote = _mapper.Map<Quote>(updateQuoteModel);
+
+            quote.Id.Should().Be(updateQuoteModel.Id);
+            quote.ClientName.Should().Be(updateQuoteModel.ClientName);
+            quote.ClientSex.Should().Be((int)updateQuoteModel.ClientSex);
+            quote.DateofBirth.Should().Be(updateQuoteModel.DateofBirth);
+            quote.Email.Should().Be(updateQuoteModel.Email);
+            quote.MobileNumber.Should().Be(updateQuoteModel.MobileNumber);
+            quote.InvestmentAmount.Should().Be(updateQuoteModel.InvestmentAmount);
+            quote.PensionPlan.Should().Be((int)updateQuoteModel.PensionPlan);
+            quote.RetirementAge.Should().Be(updateQuoteModel.RetirementAge);
+        }
+
+        [Test]
+        public void Map_Quote_ResponseQuoteModel()
+        {
+            var responseQuoteModel = _mapper.Map<ResponseQuoteModel>(quoteEntity);
+
+            responseQuoteModel.Id.Should().Be(quoteEntity.Id);
+            responseQuoteModel.ClientName.Should().Be(quoteEntity.ClientName);
+            responseQuoteModel.ClientSex.Should().Be(quoteEntity.ClientSex);
+            responseQuoteModel.DateofBirth.Should().Be(quoteEntity.DateofBirth);
+            responseQuoteModel.Email.Should().Be(quoteEntity.Email);
+            responseQuoteModel.QuoteDate.Should().Be(quoteEntity.QuoteDate);
+            responseQuoteModel.MobileNumber.Should().Be(quoteEntity.MobileNumber);
+            responseQuoteModel.InvestmentAmount.Should().Be(quoteEntity.InvestmentAmount);
+            responseQuoteModel.PensionPlan.Should().Be(quoteEntity.PensionPlan);
+            responseQuoteModel.RetirementAge.Should().Be(quoteEntity.RetirementAge);
+            responseQuoteModel.MaturityAmount.Should().Be(quoteEntity.MaturityAmount);
+        }
+
+        [Test]
+        public void Map_CreateQuoteModel_Quote_PensionSilver()
+        {
+            createQuoteModel.PensionPlan = PensionPlan.PensionSilver;
+            var quote = _mapper.Map<Quote>(createQuoteModel);
+
+            quote.PensionPlan.Should().Be((int)createQuoteModel.PensionPlan);
+        }
+
+        [Test]
+        public void Map_CreateQuoteModel_Quote_PensionGold()
+        {
+            createQuoteModel.PensionPlan = PensionPlan.PensionGold;
+            var quote = _mapper.Map<Quote>(createQuoteModel);
+
+            quote.PensionPlan.Should().Be((int)createQuoteModel.PensionPlan);
+        }
+    }
+}
