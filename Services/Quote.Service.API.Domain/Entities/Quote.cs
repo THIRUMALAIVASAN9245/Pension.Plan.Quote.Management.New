46e30254df97f3334fe8b787aa71ceae9f5d1890
Thirumalaivasan S <THIRUMALAIVASAN9245@GMAIL.COM>
2020-09-03 19:40:39 +0530

    Add files via upload

---
+++ Services/Quote.Service.API.Domain/Entities/Quote.cs
@@ -0,0 +1,29 @@
+﻿namespace Quote.Service.API.Domain.Entities
+{
+    using System;
+
+    public partial class Quote
+    {
+        public Guid Id { get; set; }
+
+        public string ClientName { get; set; }
+
+        public int ClientSex { get; set; }
+
+        public DateTime? DateofBirth { get; set; }
+
+        public DateTime? QuoteDate { get; set; }
+
+        public string Email { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public decimal InvestmentAmount { get; set; }
+
+        public int RetirementAge { get; set; }
+
+        public int PensionPlan { get; set; }
+
+        public decimal MaturityAmount { get; set; }
+    }
+}
